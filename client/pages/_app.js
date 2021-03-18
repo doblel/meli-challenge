0f30569564eba18dba0cf68e46b14ed67309c5a2
Lisandro <lucattilisandro@gmail.com>
2021-03-18 11:57:41 -0300

    [ADD] Progress bar

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,6 +2,18 @@ import '../styles/_globals.scss'
 import { AppContextProvider } from "../context"
 import Layout from '../components/Layout'
 
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#3483fa",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 const App = ({ Component, pageProps }) => {
   return (
